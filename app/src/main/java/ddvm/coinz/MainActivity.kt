3bcca97a2f9d2fbe360d4188cf166a878006d882
vmdd <dawidduc@hotmail.com>
2018-10-26 21:08:38 +0100

    added location functionality

--- app/src/main/java/ddvm/coinz/MainActivity.kt
+++ app/src/main/java/ddvm/coinz/MainActivity.kt
@@ -2,25 +2,39 @@ package ddvm.coinz
 
 import android.location.Location
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationEngineListener, PermissionsListener {
 
+    private val tag = "MainActivity"
     private var mapView: MapView? = null
+    private var map: MapboxMap? = null
+
+    private lateinit var originLocation: Location
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var locationEngine: LocationEngine
+    private lateinit var locationLayerPlugin: LocationLayerPlugin
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +44,107 @@ class MainActivity : AppCompatActivity() {
         Mapbox.getInstance(this, getString(R.string.access_token))
         mapView = findViewById(R.id.mapboxMapView)
         mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        if(mapboxMap == null){
+            Log.d(tag, "[onMapReady] mapboxMap is null")
+        }else{
+            map = mapboxMap
+            //Set user interface options
+            map?.uiSettings?.isCompassEnabled = true
+            map?.uiSettings?.isZoomControlsEnabled = true
+
+            //make location information avaliable
+            enableLocation()
+
+        }
+    }
+
+    private fun enableLocation() {
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            Log.d(tag, "Permissions are granted")
+            initialiseLocationEngine()
+            initialiseLocationLayer()
+        }else{
+            Log.d(tag, "Permissions are not granted")
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    private fun initialiseLocationEngine() {
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine.apply{
+            interval = 5000 //every 5 seconds
+            fastestInterval = 1000 //at most every second
+            priority = LocationEnginePriority.HIGH_ACCURACY
+            activate()
+        }
+        val lastLocation = locationEngine.lastLocation
+        if(lastLocation != null){
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        }else{
+            locationEngine.addLocationEngineListener(this)
+        }
+    }
+
+    private fun setCameraPosition(location: Location) {
+        val latlng = LatLng(location.latitude, location.longitude)
+        map?.animateCamera(CameraUpdateFactory.newLatLng(latlng))
+    }
+
+    private fun initialiseLocationLayer() {
+        if (mapView == null) { Log.d(tag, "mapView is null") }
+        else{
+            if (map == null) { Log.d(tag, "map is null") }
+            else{
+                locationLayerPlugin = LocationLayerPlugin(mapView!!, map!!, locationEngine)
+                locationLayerPlugin.apply{
+                    setLocationLayerEnabled(true)
+                    cameraMode = CameraMode.TRACKING
+                    renderMode = RenderMode.NORMAL
+                }
+            }
+        }
+    }
+
+
+
+    override fun onLocationChanged(location: Location?) {
+        if(location == null){
+            Log.d(tag, "[onLocationChanged] location is null")
+        }else{
+            originLocation = location
+            setCameraPosition(originLocation)
+        }
+    }
+
+    override fun onConnected() {
+        Log.d(tag, "[onConnected] requesting location updates")
+        locationEngine.requestLocationUpdates()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        Log.d(tag, "[onPermissionResult] granted == $granted")
+        if (granted) {
+            enableLocation()
+        } else {
+            // Open a dialogue with the user
+        }
+
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Log.d(tag, "Permissions: $permissionsToExplain")
+        // Present popup message or dialog
     }
 
     override fun onStart() {
