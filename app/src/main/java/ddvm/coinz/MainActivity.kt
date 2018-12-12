e3a822463cc8e75e07de359946045627c8d85a53
vmdd <ddvm@hotmail.com>
2018-12-12 17:56:47 +0000

    UI improvements

--- app/src/main/java/ddvm/coinz/MainActivity.kt
+++ app/src/main/java/ddvm/coinz/MainActivity.kt
@@ -14,7 +14,6 @@ import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -643,18 +642,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationEngineList
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
             android.R.id.home -> {
                 drawer_layout.openDrawer(GravityCompat.START)
                 true
